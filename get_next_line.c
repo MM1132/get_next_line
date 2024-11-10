59dd0e9684cf6037b1c0c9bc9731e016738d0106
Robert Reimann <rreimann@1-A-32.42heilbronn.de>
2024-11-10 17:55:01 +0100

    Almost all norminette cases fixed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rreimann <rreimann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:21:33 by rreimann          #+#    #+#             */
-/*   Updated: 2024/11/10 16:58:06 by rreimann         ###   ########.fr       */
+/*   Updated: 2024/11/10 17:51:14 by rreimann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,26 @@
 
 #include "get_next_line.h"
 
+static char	*compose_and_free(char main_buffer[BUFFER_SIZE], char *temp_buffer)
+{
+	char	*line;
+
+	line = compose_line_from_buffers(main_buffer, temp_buffer);
+	return (free(temp_buffer), line);
+}
+
+static int	either_buffer_contains_new_line(char main_buffer[BUFFER_SIZE],
+			char *temp_buffer)
+{
+	return (get_new_line_index(main_buffer, BUFFER_SIZE) != -1 || \
+		get_new_line_index(temp_buffer, string_length(temp_buffer, -1)) != -1);
+}
+
 static char	*general_checking_loop(int fd, char main_buffer[BUFFER_SIZE])
 {
 	char	read_buffer[BUFFER_SIZE];
 	char	*temp_buffer;
-	char	*line;
 	ssize_t	read_status;
-	size_t	add_status;
 
 	temp_buffer = (char *)malloc(sizeof(char));
 	if (temp_buffer == NULL)
@@ -30,52 +43,30 @@ static char	*general_checking_loop(int fd, char main_buffer[BUFFER_SIZE])
 	temp_buffer[0] = 0;
 	while (1)
 	{
-		//printf(" - While loop starting\n");
-		if (get_new_line_index(main_buffer, BUFFER_SIZE) != -1 ||\
-			get_new_line_index(temp_buffer, string_length(temp_buffer, -1)) != -1)
-		{
-			//printf(" - Either of the buffers contains a new line\n");
-			//printf(":main_buffer: '%s'\n", main_buffer);
-			//printf(":temp_buffer: '%s'\n", temp_buffer);
-
-			//printf(" - Composing new line from buffers...\n");
-			line = compose_line_from_buffers(main_buffer, temp_buffer);
-			//printf(":main_buffer: '%s'\n", main_buffer);
-			return (free(temp_buffer), line);
-		}
+		if (either_buffer_contains_new_line(main_buffer, temp_buffer))
+			return (compose_and_free(main_buffer, temp_buffer));
 		else
 		{
-			//printf(" - Buffers didn't contain new lines, reading...\n");
-
 			read_status = read(fd, read_buffer, BUFFER_SIZE);
-			// If reading worked, add characters to the temp buffer
 			if (read_status >= 0 && read_status <= BUFFER_SIZE)
-			{
-				//printf(" - We read %zd bytes\n", read_status);
-
-				add_status = add_read_characters_to_buffer(&temp_buffer, read_buffer, (size_t)read_status);
-				if (add_status == (size_t)-1)
+				if (add_to_buffer(&temp_buffer,
+						read_buffer, (size_t)read_status) == (size_t) - 1)
 					return (free(temp_buffer), NULL);
-				//printf(":temp_buffer after adding: '%s'\n", temp_buffer);
-			}
-			// If we read less than the BUFFER_SIZE, which means that we reached EOF
-			// Then get the line and return it
 			if (read_status >= 0 && read_status < BUFFER_SIZE)
-			{
-				//printf(" - We read less than BUFFER_SIZE, getting line...\n");
-
-				line = compose_line_from_buffers(main_buffer, temp_buffer);
-				return (free(temp_buffer), line);
-			}
-			// in case of a read error
-			// return NULL
+				return (compose_and_free(main_buffer, temp_buffer));
 			else if (read_status == -1)
-			{
-				//printf(" - Oh no! There was a read error, returning NULL...\n");
 				return (free(temp_buffer), NULL);
-			}
 		}
 	}
+}
+
+char	*return_line(char *line, char main_buffer[BUFFER_SIZE],
+			char *temp_buffer, size_t length)
+{
+	add_to_buffer(&line, main_buffer, BUFFER_SIZE);
+	add_to_buffer(&line, temp_buffer, length);
+	set_remaining_buffer(main_buffer, temp_buffer);
+	return (line);
 }
 
 char	*get_next_line(int fd)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rreimann <rreimann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:21:56 by rreimann          #+#    #+#             */
-/*   Updated: 2024/11/10 16:54:53 by rreimann         ###   ########.fr       */
+/*   Updated: 2024/11/10 17:54:26 by rreimann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ size_t	string_length(char *str, size_t max_len)
 	length = 0;
 	while (str[length] != 0)
 	{
-		if (max_len != (size_t)-1 && length >= max_len)
+		if (max_len != (size_t) - 1 && length >= max_len)
 			return (length);
 		length++;
 	}
@@ -49,43 +49,38 @@ size_t	string_length(char *str, size_t max_len)
 // Return -1 if anything fails
 // Otherwise return the number of characters that are now in the buffer
 // We expect buffer not to be NULL
-size_t	add_read_characters_to_buffer(char **buffer, char *add_buffer, size_t add_buffer_length)
+size_t	add_to_buffer(char **buffer,
+			char *add_buffer, size_t add_buffer_length)
 {
 	char	*new_buffer;
 	size_t	old_buffer_length;
 	size_t	index;
+	size_t	memory_allocation_size;
 
 	if (add_buffer == NULL || buffer == NULL)
 		return (-1);
 	old_buffer_length = string_length(*buffer, -1);
-	// So we allocate for the new buffer
-	size_t memory_allocation_size = old_buffer_length + add_buffer_length + 1;
+	memory_allocation_size = old_buffer_length + add_buffer_length + 1;
 	new_buffer = (char *)malloc(sizeof(char) * memory_allocation_size);
 	if (new_buffer == NULL)
 		return (-1);
-	// Add things from the old buffer
 	index = 0;
-	while (index < old_buffer_length)
-	{
-		new_buffer[index] = (*buffer)[index];
+	while (new_buffer[index] = (*buffer)[index], index < old_buffer_length)
 		index++;
-	}
-	// Add things from the add buffer
 	index = 0;
 	while (index < add_buffer_length)
 	{
 		new_buffer[old_buffer_length + index] = add_buffer[index];
 		index++;
 	}
-	// Add the null terminator
 	new_buffer[old_buffer_length + add_buffer_length] = 0;
-	// Finally do the memory things and return
 	free(*buffer);
 	*buffer = new_buffer;
 	return (old_buffer_length + add_buffer_length);
 }
 
-static void	set_remaining_buffer(char main_buffer[BUFFER_SIZE], char *temp_buffer)
+void	set_remaining_buffer(char main_buffer[BUFFER_SIZE],
+			char *temp_buffer)
 {
 	int	main_new_line_index;
 	int	temp_new_line_index;
@@ -99,23 +94,17 @@ static void	set_remaining_buffer(char main_buffer[BUFFER_SIZE], char *temp_buffe
 		main_buffer[index] = main_buffer[index + main_new_line_index + 1];
 		index++;
 	}
-	//printf(":main_buffer 1: '%s'\n", main_buffer);
 	temp_buffer_length = string_length(temp_buffer, -1);
 	temp_new_line_index = get_new_line_index(temp_buffer, temp_buffer_length);
 	if (temp_new_line_index == -1)
 		temp_new_line_index = temp_buffer_length - 1;
-	// "Thank you for this amazing evaluation\nIt w" => 37, length: 42
-	//printf(" - Setting remaining characters for the main_buffer...\n");
 	while (index + temp_new_line_index + 1 < temp_buffer_length)
 	{
-		//printf(":temp_buffer[%d]: %c\n", index, temp_buffer[index + temp_new_line_index + 1]);
 		main_buffer[index] = temp_buffer[index + temp_new_line_index + 1];
 		index++;
 	}
-	//printf(":main_buffer 2: '%s'\n", main_buffer);
 	while (index < BUFFER_SIZE)
 		main_buffer[index++] = 0;
-	//printf(":main_buffer 3: '%s'\n", main_buffer);
 }
 
 // This should put the two buffers together and return the line
@@ -128,35 +117,19 @@ char	*compose_line_from_buffers(char main_buffer[BUFFER_SIZE], char *temp_buffer
 
 	if (string_length(main_buffer, BUFFER_SIZE) == 0 && string_length(temp_buffer, -1) == 0)
 		return (NULL);
-
-	// Allocate memory for the empty line
 	line = (char *)malloc(sizeof(char));
 	if (line == NULL)
 		return (NULL);
 	line[0] = 0;
-
 	main_new_line_index = get_new_line_index(main_buffer, BUFFER_SIZE);
 	temp_new_line_index = get_new_line_index(temp_buffer, string_length(temp_buffer, -1));
-	// If there is a new line in the main buffer
 	if (main_new_line_index != -1)
 	{
-		add_read_characters_to_buffer(&line, main_buffer, main_new_line_index + 1);
-		set_remaining_buffer(main_buffer, temp_buffer);
-		return (line);
+		add_to_buffer(&line, main_buffer, main_new_line_index + 1);
+		return (set_remaining_buffer(main_buffer, temp_buffer), line);
 	}
 	else if (temp_new_line_index != -1)
-	{
-		add_read_characters_to_buffer(&line, main_buffer, BUFFER_SIZE);
-		add_read_characters_to_buffer(&line, temp_buffer, temp_new_line_index + 1);
-		set_remaining_buffer(main_buffer, temp_buffer);
-		return (line);
-	}
+		return (return_line(line, main_buffer, temp_buffer, temp_new_line_index + 1));
 	else
-	{
-		add_read_characters_to_buffer(&line, main_buffer, BUFFER_SIZE);
-		add_read_characters_to_buffer(&line, temp_buffer, string_length(temp_buffer, -1));
-		set_remaining_buffer(main_buffer, temp_buffer);
-		return (line);
-	}
-	return (free(line), NULL);
+		return (return_line(line, main_buffer, temp_buffer, string_length(temp_buffer, -1)));
 }
