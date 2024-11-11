7f79500771bc32c4eb435905618a381fc786b656
Robert Reimann <rreimann@3-E-20.42heilbronn.de>
2024-11-11 12:50:55 +0100

    norminette and paco passing

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rreimann <rreimann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:21:56 by rreimann          #+#    #+#             */
-/*   Updated: 2024/11/10 17:57:18 by rreimann         ###   ########.fr       */
+/*   Updated: 2024/11/11 12:50:43 by rreimann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,51 +50,47 @@ size_t	string_length(char *str, size_t max_len)
 // Otherwise return the number of characters that are now in the buffer
 // We expect buffer not to be NULL
 size_t	add_to_buffer(char **buffer,
-			char *add_buffer, size_t add_buffer_length)
+			char *add_buffer, size_t add_len)
 {
 	char	*new_buffer;
-	size_t	old_buffer_length;
+	size_t	buf_len;
 	size_t	index;
-	size_t	memory_allocation_size;
 
-	if (add_buffer == NULL || buffer == NULL)
-		return (-1);
-	old_buffer_length = string_length(*buffer, -1);
-	memory_allocation_size = old_buffer_length + add_buffer_length + 1;
-	new_buffer = (char *)malloc(sizeof(char) * memory_allocation_size);
+	buf_len = string_length(*buffer, -1);
+	new_buffer = (char *)malloc(sizeof(char) * (buf_len + add_len + 1));
 	if (new_buffer == NULL)
 		return (-1);
 	index = 0;
-	while (index < old_buffer_length)
+	while (index < buf_len)
 	{
 		new_buffer[index] = (*buffer)[index];
 		index++;
 	}
 	index = 0;
-	while (index < add_buffer_length)
+	while (index < add_len)
 	{
-		new_buffer[old_buffer_length + index] = add_buffer[index];
+		new_buffer[buf_len + index] = add_buffer[index];
 		index++;
 	}
-	new_buffer[old_buffer_length + add_buffer_length] = 0;
+	new_buffer[buf_len + add_len] = 0;
 	free(*buffer);
 	*buffer = new_buffer;
-	return (old_buffer_length + add_buffer_length);
+	return (buf_len + add_len);
 }
 
 void	set_remaining_buffer(char main_buffer[BUFFER_SIZE],
 			char *temp_buffer)
 {
-	int	main_new_line_index;
+	int	buf_nli;
 	int	temp_new_line_index;
 	int	temp_buffer_length;
 	int	index;
 
-	main_new_line_index = get_new_line_index(main_buffer, BUFFER_SIZE);
+	buf_nli = get_new_line_index(main_buffer, BUFFER_SIZE);
 	index = 0;
-	while (main_new_line_index != -1 && index + main_new_line_index + 1 < BUFFER_SIZE)
+	while (buf_nli != -1 && index + buf_nli + 1 < BUFFER_SIZE)
 	{
-		main_buffer[index] = main_buffer[index + main_new_line_index + 1];
+		main_buffer[index] = main_buffer[index + buf_nli + 1];
 		index++;
 	}
 	temp_buffer_length = string_length(temp_buffer, -1);
@@ -112,27 +108,32 @@ void	set_remaining_buffer(char main_buffer[BUFFER_SIZE],
 
 // This should put the two buffers together and return the line
 // Keep the remaining buffer in the main buffer
-char	*compose_line_from_buffers(char main_buffer[BUFFER_SIZE], char *temp_buffer)
+char	*compose_line_from_buffers(char main_buffer[BUFFER_SIZE],
+			char *temp_buffer)
 {
 	char	*line;
 	int		main_new_line_index;
 	int		temp_new_line_index;
 
-	if (string_length(main_buffer, BUFFER_SIZE) == 0 && string_length(temp_buffer, -1) == 0)
+	if (string_length(main_buffer, BUFFER_SIZE) == 0 && \
+				string_length(temp_buffer, -1) == 0)
 		return (NULL);
 	line = (char *)malloc(sizeof(char));
 	if (line == NULL)
 		return (NULL);
 	line[0] = 0;
 	main_new_line_index = get_new_line_index(main_buffer, BUFFER_SIZE);
-	temp_new_line_index = get_new_line_index(temp_buffer, string_length(temp_buffer, -1));
+	temp_new_line_index = get_new_line_index(temp_buffer,
+			string_length(temp_buffer, -1));
 	if (main_new_line_index != -1)
 	{
 		add_to_buffer(&line, main_buffer, main_new_line_index + 1);
 		return (set_remaining_buffer(main_buffer, temp_buffer), line);
 	}
 	else if (temp_new_line_index != -1)
-		return (return_line(line, main_buffer, temp_buffer, temp_new_line_index + 1));
+		return (return_line(line, main_buffer, temp_buffer,
+				temp_new_line_index + 1));
 	else
-		return (return_line(line, main_buffer, temp_buffer, string_length(temp_buffer, -1)));
+		return (return_line(line, main_buffer, temp_buffer,
+				string_length(temp_buffer, -1)));
 }
